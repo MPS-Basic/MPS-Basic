108449337e834172f02210f64c2375d186a27767
Kyuya MATSUMOTO <qmatsumoto@g.ecc.u-tokyo.ac.jp>
2023-07-10 19:47:15 +0900

    add: automatic initialize and test code of loader

--- src/loader.hpp
+++ src/loader.hpp
@@ -28,7 +28,8 @@ private:
 
 public:
 	const VariableType type;
-	bool hasLoaded = false;
+	bool hasInitialValue = false;
+	bool hasLoaded       = false;
 
 	VariableData(int* p) : type(VariableType::Int) {
 		pint = p;
@@ -86,29 +87,50 @@ public:
 			cerr << "identifier " + name + " has already been defined" << endl;
 			exit(-1);
 		}
-		vars.insert(name, VariableData(pointer));
+		vars.emplace(name, VariableData(pointer));
+	}
+
+	template <class T>
+	auto addDefinition(string name, T* pointer, T defaultValue)
+	    -> decltype(VariableData(pointer), void()) {
+		addDefinition(name, pointer);
+		vars.at(name).set(defaultValue);
+		vars.at(name).hasInitialValue = true;
 	}
 
 	void load(string path) {
-		for (auto& v : vars) {
-			v.second.hasLoaded = false;
-		}
 		ifstream in(path);
 		if (in.fail()) {
 			cerr << "could not load file: " + path << endl;
 			exit(-1);
 		}
+		load(in);
+	}
+
+	void load(istream& in) {
 		string line;
 		while (getline(in, line)) {
 			parseLine(line);
+		}
+		bool ng = false;
+		for (auto& e : vars) {
+			if (!e.second.hasInitialValue && !e.second.hasLoaded) {
+				cerr << "uninitialized variable: " + e.first << endl;
+				ng = true;
+			}
+		}
+		if (ng) {
+			exit(-1);
 		}
 	}
 
 private:
 	void parseLine(string line) {
-		auto commentRegex = regex("\\s*#.*");
-		if (regex_match(line, commentRegex)) {
-			return; // skipping the comment
+		if (regex_match(line, regex("\\s*"))) {
+			return; // skipping the empty line
+		}
+		if (regex_match(line, regex("\\s*#.*"))) {
+			return; // skipping the comment line
 		}
 		smatch match;
 		auto definitionRegex =
@@ -117,31 +139,45 @@ private:
 			string name  = match[1];
 			string value = match[2];
 			if (vars.count(name) == 0) {
-				cout << "[WARN] " + name +
+				cerr << "[WARN] " + name +
 				            " is found, but not defined. skipping..."
 				     << endl;
 				return;
 			}
-			auto data = vars[name];
+			auto& data = vars.at(name);
+			if (data.hasLoaded) {
+				cerr << "duplicate definition of " + name + " in file" << endl;
+				exit(-1);
+			}
+			data.hasLoaded = true;
 			switch (data.type) {
 			case VariableType::Int:
 				data.set(parseInt(value));
-				break;
+				return;
 			case VariableType::Double:
 				data.set(parseDouble(value));
-				break;
+				return;
 			case VariableType::Bool:
 				data.set(parseBool(value));
-				break;
+				return;
 			case VariableType::String:
 				data.set(parseString(value));
-				break;
+				return;
 			}
+			cerr << "invalid variable type: " << (int) data.type << endl;
+			exit(-1);
+			return;
 		}
+		cerr << "syntax error: " + line << endl;
+		exit(-1);
 	}
 
 	int parseInt(string str) {
-		return stoi(str);
+		if (str.find('.') != string::npos || str.find("e-") != string::npos) {
+			cerr << "could not parse " + str + " as an integer" << endl;
+			exit(-1);
+		}
+		return stoi(str, nullptr, 0);
 	}
 
 	double parseDouble(string str) {
@@ -165,12 +201,8 @@ private:
 		string quotationError =
 		    "strings must be enclosed in double quotations: " + str;
 		string invalidEscapeError = "an invalid escape sequence found: " + str;
-		if (len < 2) {
-			cerr << quotationError << endl;
-			exit(-1);
-		}
-		bool quotationOpened = false;
-		bool quotationClosed = false;
+		bool quotationOpened      = false;
+		bool quotationClosed      = false;
 		rep(i, 0, len) {
 			if (i == 0 && str[i] == '"') {
 				quotationOpened = true;
@@ -208,12 +240,15 @@ private:
 			}
 			res << str[i];
 		}
+		if (!quotationOpened || !quotationClosed) {
+			cerr << quotationError << endl;
+			exit(-1);
+		}
+		return res.str();
 	}
 };
 
 } // namespace loader
 
 // expose data
-using VariableType = loader::VariableType;
-using VariableData = loader::VariableData;
-using Loader       = loader::Loader;
+using Loader = loader::Loader;
