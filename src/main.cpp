58f5edb3163ecb7f14447063eee08f4bb529f995
Kyuya Matsumoto <qmatsumoto@g.ecc.u-tokyo.ac.jp>
2024-02-13 20:56:10 +0900

    update: settings path provided as an argument

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,19 @@
-#include "mps.hpp"
 #include "input.hpp"
 #include "loader.hpp"
+#include "mps.hpp"
 #include <filesystem>
 
 int main(int argc, char** argv) {
-	auto settingPath = std::filesystem::path("./input/settings.yml");
-	Input input = Loader().load(settingPath);
-	MPS mps = MPS(input);
+	if (argc < 2) {
+		std::cout << "Usage" << std::endl << std::endl;
+		std::cout << "  mps <path-to-settings>" << std::endl << std::endl;
+		std::cout << "Specify a settings for simulation." << std::endl;
+		return 0;
+	}
+
+	auto settingPath = std::filesystem::path(argv[1]);
+	Input input      = Loader().load(settingPath);
+	MPS mps          = MPS(input);
 	mps.run();
 
 	return 0;
