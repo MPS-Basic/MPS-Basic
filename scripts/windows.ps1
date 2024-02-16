aa57fdfa30b945e7c5af5304559a28c85d2ad12c
Kyuya Matsumoto <qmatsumoto@g.ecc.u-tokyo.ac.jp>
2024-02-16 23:18:54 +0900

    update: powershell script is independent from cwd

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -1,3 +1,4 @@
-del result\prof\*.prof
-del result\vtu\*.vtu
-.\build\mps.exe 2>result\error.log
+$rootPath = (get-item $PSScriptRoot).parent.FullName
+del $rootPath\result\dambreak\*.prof
+del $rootPath\result\dambreak\*.vtu
+$rootPath\build\mps.exe 2>$rootPath\result\error.log
