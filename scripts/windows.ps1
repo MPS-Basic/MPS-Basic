ec0101d04c81ef93ff6de049badd1ceef56e2d62
iori3150 <iori3150jp@gmail.com>
2024-02-18 00:21:50 +0900

    update windows.ps1

--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -1,5 +1,5 @@
-# set root path so that we can activate the script from any directory
-$rootPath = (get-item $PSScriptRoot).parent.FullName 
+# go to root directory so that we can activate the script from any directory
+cd (get-item $PSScriptRoot).parent.FullName 
 
-Get-ChildItem $rootPath/result/dambreak -Include *.* -Recurse | del # remove all folders/files in result/dambreak
-.$rootPath/build/mps.exe $rootPathinput/dambreak/settings.yml 2> $rootPath/result/dambreak/error.log | Tee-Object -FilePath "result/dambreak/console.log" # run simulation
+Get-ChildItem result/dambreak -Include *.* -Recurse | del # remove all folders/files in result/dambreak
+./build/mps.exe input/dambreak/settings.yml 2> result/dambreak/error.log | Tee-Object -FilePath "result/dambreak/console.log" # run simulation
