dc8a15dd0f5ac16ba48a178f4048915ac72a78b4
iori3150 <iori3150jp@gmail.com>
2024-02-18 00:06:54 +0900

    add comment

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
-cd $(dirname $0)/.. # go to root directory
+#!/bin/bash # Shebang: to run the script with bash, not other shell like sh
+
+# go to root directory so that we can activate the script from any directory
+cd $(dirname $0)/.. 
+
 mkdir -p result/dambreak/
 ./build/mps input/dambreak/settings.yml 2>result/dambreak/error.log # run simulation
--- scripts/windows.ps1
+++ scripts/windows.ps1
@@ -1,4 +1,7 @@
-$rootPath = (get-item $PSScriptRoot).parent.FullName
+# set root path so that we can activate the script from any directory
+$rootPath = (get-item $PSScriptRoot).parent.FullName 
+
+echo "Root path: $rootPath"
 del $rootPath\result\dambreak\*.prof
 del $rootPath\result\dambreak\*.vtu
-$rootPath\build\mps.exe 2>$rootPath\result\error.log
+.$rootPath\build\mps.exe 2>$rootPath\result\error.log
