0c9f3748c391ff25e84919233ac00024644e71c8
Queue_sys <qdevelop@hotmail.com>
2024-02-23 06:08:37 +0900

    enable test for RefValues

--- test/refvalues_test.cpp
+++ test/refvalues_test.cpp
@@ -3,48 +3,25 @@
 #include <cmath>
 #include <gtest/gtest.h>
 
-TEST(RefValuesTest, NumberDensityForGradientTest2d) {
-	double l0                  = 0.1;
-	double re_forNumberDensity = 2.5 * l0;
-	double re_forGradient      = 1.5 * l0;
-	double re_forLaplacian     = 3.1 * l0;
-	RefValues ref;
-	ref.calc(2, l0, re_forNumberDensity, re_forGradient, re_forLaplacian);
+TEST(RefValuesTest, test2d) {
+	double l0 = 0.1;
+	double re = 1.5 * l0;
+	RefValues ref(2, l0, re);
 
-	double n0_exact = 4.0 * weight(l0, re_forGradient) + 4.0 * weight(sqrt(2.0) * l0, re_forGradient);
+	double n0_exact = 4.0 * weight(l0, re) + 4.0 * weight(sqrt(2.0) * l0, re);
+	double lambda_exact = (4.0 *(l0*l0)* weight(l0, re) + 4.0 *(2.0*l0*l0)* weight(sqrt(2.0) * l0, re)) / n0_exact;
 
-	EXPECT_DOUBLE_EQ(ref.n0_forGradient, n0_exact);
+	EXPECT_DOUBLE_EQ(ref.n0, n0_exact);
+	EXPECT_DOUBLE_EQ(ref.lambda, lambda_exact);
 }
-TEST(RefValuesTest, NumberDensityForGradientTest3d) {
-	double l0                  = 0.1;
-	double re_forNumberDensity = 2.5 * l0;
-	double re_forGradient      = 1.5 * l0;
-	double re_forLaplacian     = 3.1 * l0;
-	RefValues ref;
-	ref.calc(3, l0, re_forNumberDensity, re_forGradient, re_forLaplacian);
+TEST(RefValuesTest, test3d) {
+	double l0 = 0.1;
+	double re = 1.5 * l0;
+	RefValues ref(3, l0, re);
 
-	double n0_exact = 6.0 * weight(l0, re_forGradient) + 12.0 * weight(sqrt(2.0) * l0, re_forGradient);
-	EXPECT_DOUBLE_EQ(ref.n0_forGradient, n0_exact);
-}
-TEST(RefValuesTest, NumberDensityForLaplacianTest2d) {
-	double l0                  = 0.1;
-	double re_forNumberDensity = 2.5 * l0;
-	double re_forGradient      = 2.1 * l0;
-	double re_forLaplacian     = 1.5 * l0;
-	RefValues ref;
-	ref.calc(2, l0, re_forNumberDensity, re_forGradient, re_forLaplacian);
+	double n0_exact = 6.0 * weight(l0, re) + 12.0 * weight(sqrt(2.0) * l0, re);
+	double lambda_exact = (6.0 *(l0*l0)* weight(l0, re) + 12.0 *(2.0*l0*l0)* weight(sqrt(2.0) * l0, re)) / n0_exact;
 
-	double n0_exact = 4.0 * weight(l0, re_forLaplacian) + 4.0 * weight(sqrt(2.0) * l0, re_forLaplacian);
-	EXPECT_DOUBLE_EQ(ref.n0_forLaplacian, n0_exact);
-}
-TEST(RefValuesTest, NumberDensityForLaplacianTest3d) {
-	double l0                  = 0.1;
-	double re_forNumberDensity = 2.5 * l0;
-	double re_forGradient      = 2.1 * l0;
-	double re_forLaplacian     = 1.5 * l0;
-	RefValues ref;
-	ref.calc(3, l0, re_forNumberDensity, re_forGradient, re_forLaplacian);
-
-	double n0_exact = 6.0 * weight(l0, re_forLaplacian) + 12.0 * weight(sqrt(2.0) * l0, re_forLaplacian);
-	EXPECT_DOUBLE_EQ(ref.n0_forLaplacian, n0_exact);
+	EXPECT_DOUBLE_EQ(ref.n0, n0_exact);
+	EXPECT_DOUBLE_EQ(ref.lambda, lambda_exact);
 }
