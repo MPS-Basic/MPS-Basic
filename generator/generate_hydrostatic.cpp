27b5d875f618024346e3ce3246672377193f69c2
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-02-25 17:17:10 +0900

    format code

--- generator/generate_hydrostatic.cpp
+++ generator/generate_hydrostatic.cpp
@@ -1,5 +1,6 @@
 #include "../src/particle.hpp"
 #include "../src/particles_exporter.hpp"
+
 #include <algorithm>
 #include <array>
 #include <filesystem>
@@ -31,44 +32,44 @@ int main(int argc, char** argv) {
 
     double l0 = 0.012;
 
-	Box2d fluidDomain(-0.18, 0.18, 0, 0.48);
-	Box2d emptyDomain(-0.18, 0.18, 0.48, 0.6);
-	Box2d wallBB(-0.18 - 2 * l0, 0.18 + 2 * l0, -2 * l0, 0.6);
-	Box2d dummyBB(-0.18 - 4 * l0, 0.18 + 4 * l0, -4 * l0, 0.6);
+    Box2d fluidDomain(-0.18, 0.18, 0, 0.48);
+    Box2d emptyDomain(-0.18, 0.18, 0.48, 0.6);
+    Box2d wallBB(-0.18 - 2 * l0, 0.18 + 2 * l0, -2 * l0, 0.6);
+    Box2d dummyBB(-0.18 - 4 * l0, 0.18 + 4 * l0, -4 * l0, 0.6);
 
-	int id = 0;
-	for (int ix = (int) round(-0.18 / l0) - 4; ix < (int) round(0.18 / l0) + 4; ix++) {
-		for (int iy = (int) round(0 / l0) - 4; iy < (int) round(0.6 / l0); iy++) {
-			Eigen::Vector2d r(l0 * (ix + 0.5), l0 * (iy + 0.5));
-			Eigen::Vector3d r_3d(r.x(), r.y(), 0);
+    int id = 0;
+    for (int ix = (int) round(-0.18 / l0) - 4; ix < (int) round(0.18 / l0) + 4; ix++) {
+        for (int iy = (int) round(0 / l0) - 4; iy < (int) round(0.6 / l0); iy++) {
+            Eigen::Vector2d r(l0 * (ix + 0.5), l0 * (iy + 0.5));
+            Eigen::Vector3d r_3d(r.x(), r.y(), 0);
 
-			// empty region
-			if (emptyDomain.sdf(r) < 0) {
-				continue;
-			}
-			// fluid region
-			if (fluidDomain.sdf(r) < 0) {
-				particles.push_back(Particle(id, ParticleType::Fluid, r_3d, Eigen::Vector3d::Zero()));
-				id++;
-				continue;
-			}
-			// wall region
-			if (wallBB.sdf(r) < 0) {
-				particles.push_back(Particle(id, ParticleType::Wall, r_3d, Eigen::Vector3d::Zero()));
-				id++;
-				continue;
-			}
-			// dummy region
-			if (dummyBB.sdf(r) < 0) {
-				particles.push_back(Particle(id, ParticleType::DummyWall, r_3d, Eigen::Vector3d::Zero()));
-				id++;
-				continue;
-			}
-		}
-	}
+            // empty region
+            if (emptyDomain.sdf(r) < 0) {
+                continue;
+            }
+            // fluid region
+            if (fluidDomain.sdf(r) < 0) {
+                particles.push_back(Particle(id, ParticleType::Fluid, r_3d, Eigen::Vector3d::Zero()));
+                id++;
+                continue;
+            }
+            // wall region
+            if (wallBB.sdf(r) < 0) {
+                particles.push_back(Particle(id, ParticleType::Wall, r_3d, Eigen::Vector3d::Zero()));
+                id++;
+                continue;
+            }
+            // dummy region
+            if (dummyBB.sdf(r) < 0) {
+                particles.push_back(Particle(id, ParticleType::DummyWall, r_3d, Eigen::Vector3d::Zero()));
+                id++;
+                continue;
+            }
+        }
+    }
 
-	ParticlesExporter exporter;
-	exporter.setParticles(particles);
-	exporter.toProf(fs::path("input/hydrostatic/input.prof"), 0.0);
-	exporter.toVtu(fs::path("input/hydrostatic/input.vtu"), 0.0);
+    ParticlesExporter exporter;
+    exporter.setParticles(particles);
+    exporter.toProf(fs::path("input/hydrostatic/input.prof"), 0.0);
+    exporter.toVtu(fs::path("input/hydrostatic/input.vtu"), 0.0);
 }
