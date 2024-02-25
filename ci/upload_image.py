f36ba719dbd65cae502091ad8ce587c30846a8ea
Iori Saigo <88538282+iori3150@users.noreply.github.com>
2024-02-26 04:09:40 +0900

    Update upload_image.py

--- ci/upload_image.py
+++ ci/upload_image.py
@@ -4,6 +4,6 @@ import sys
 CLIENT_ID = "a0a98f5baed7b8f"
 
 im = pyimgur.Imgur(CLIENT_ID)
-print(sys.argv[0])
-uploaded_image = im.upload_image(sys.argv[0], title="tmp")
+print(sys.argv[1])
+uploaded_image = im.upload_image(sys.argv[1], title="tmp")
 print(uploaded_image.link)
