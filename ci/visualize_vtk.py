2b1a3dcdae3ecff4b978c408a9a9e0154c843499
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-02-26 09:43:38 +0000

    add Time in CI

--- ci/visualize_vtk.py
+++ ci/visualize_vtk.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 mesh = meshio.read(sys.argv[1])
 x = mesh.points[:, 0]
 y = mesh.points[:, 1]
-p  = mesh.point_data['Pressure'][:,0]
+p  = mesh.point_data['Pressure'][:, 0]
+t = mesh.field_data['Time'][0, 0]
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
-ax.set_aspect('equal','box')
-aximg =  ax.scatter(x, y, c=p, cmap='viridis',s=24)
+ax.set_aspect('equal', 'box')
+ax.set_title('Pressure at t = {:.2f}'.format(t))
+aximg =  ax.scatter(x, y, c=p, cmap='viridis', s=24)
 
 fig.colorbar(aximg)
 plt.savefig(sys.argv[2])
