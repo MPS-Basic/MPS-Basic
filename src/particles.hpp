315e5b2e8ddb00fa4121de8d477b219f87e4ca29
Kazuki Arai <zukain5@gmail.com>
2024-04-01 20:53:32 +0900

    [skip ci] enhance comment

--- src/particles.hpp
+++ src/particles.hpp
@@ -12,8 +12,26 @@ public:
     std::vector<Particle>::iterator end();
     std::vector<Particle>::const_iterator end() const;
 
+    /**
+     * @brief Get the number of particles
+     *
+     * @return int the number of particles
+     */
     int size() const;
+
+    /**
+     * @brief Add a particle to the collection
+     *
+     * @param particle the particle to add
+     */
     void add(const Particle& particle);
+
+    /**
+     * @brief Get a particle by index
+     *
+     * @param index the index of the particle
+     * @return Particle& the particle
+     */
     Particle& operator[](size_t index);
 
 private:
