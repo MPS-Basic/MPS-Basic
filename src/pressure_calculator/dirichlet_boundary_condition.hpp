c110aecdf68ddfd4ac996c7bb308ae82abd2d03a
Kazuki ARAI <zukain5@gmail.com>
2024-04-05 15:20:13 +0900

    [skip ci] add comments

--- src/pressure_calculator/dirichlet_boundary_condition.hpp
+++ src/pressure_calculator/dirichlet_boundary_condition.hpp
@@ -6,8 +6,25 @@ namespace PressureCalculator {
 
 class DirichletBoundaryCondition {
 public:
+    /**
+     * @brief Check if the boundary condition contains the particle
+     * @param id Particle id
+     * @return True if the boundary condition contains the particle
+     */
     bool contains(int id) const;
+
+    /**
+     * @brief Get the boundary condition value
+     * @param id Particle id
+     * @return Boundary condition value
+     */
     double value(int id) const;
+
+    /**
+     * @brief Set the boundary condition value
+     * @param id Particle id
+     * @param value Boundary condition value
+     */
     void set(int id, double value);
 
     DirichletBoundaryCondition() = default;
