59db0cd0c1d4886a89eba625d073f4165401a50b
Kazuki ARAI <zukain5@gmail.com>
2024-04-23 11:10:46 +0900

    [skip ci] add comments

--- src/particles_loader/prof.hpp
+++ src/particles_loader/prof.hpp
@@ -3,6 +3,11 @@
 #include "interface.hpp"
 
 namespace ParticlesLoader {
+
+/**
+ * @brief Class for loading particles from a prof file
+ * @details For more details on the prof file format, please refer to the @ref profile.
+ */
 class Prof : public Interface {
 public:
     std::pair<double, Particles> load(const fs::path& path) override;
