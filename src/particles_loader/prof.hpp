0c5dcf4e21582636b4fe24abb2ed630806223f4f
Kazuki ARAI <zukain5@gmail.com>
2024-04-26 15:25:51 +0900

    125 separate loadParticleProf function to ParticlesLoader class (#126)
    * add interface
    
    * add particles_loader/prof.*pp
    
    * 移動完了
    
    * [skip ci] add comments
    
    * レビュー対応

---
+++ src/particles_loader/prof.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "interface.hpp"
+
+namespace ParticlesLoader {
+
+/**
+ * @brief Class for loading particles from a prof file
+ * @details For more details on the prof file format, please refer to the @ref profile.
+ */
+class Prof : public Interface {
+public:
+    std::pair<double, Particles> load(const fs::path& path) override;
+    ~Prof() override;
+    Prof();
+};
+} // namespace ParticlesLoader
