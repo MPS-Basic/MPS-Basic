30c6493903a829569d01b27e44ac59b9013ecd8a
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-08-11 02:49:22 +0900

    refactor: format code

--- ci/visualize_vtk.py
+++ ci/visualize_vtk.py
@@ -11,9 +11,9 @@ t = mesh.field_data["Time"][0]
 
 fig = plt.figure(figsize=(6, 6))
 ax = fig.add_subplot(111)
-ax.set_aspect('equal', 'box')
-ax.set_title('Pressure at t = {:.2f}'.format(t))
-aximg =  ax.scatter(x, y, c=p, cmap='viridis', s=24)
+ax.set_aspect("equal", "box")
+ax.set_title("Pressure at t = {:.2f}".format(t))
+aximg = ax.scatter(x, y, c=p, cmap="viridis", s=24)
 
 fig.colorbar(aximg)
 plt.savefig(sys.argv[2])
