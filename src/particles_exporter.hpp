a5dd7d135a8f704f2d0bc3c3acb8e774bc1cdd27
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-08-11 15:38:19 +0900

    refactor: add comment methods in ParticlesExporter

--- src/particles_exporter.hpp
+++ src/particles_exporter.hpp
@@ -20,8 +20,21 @@ class ParticlesExporter {
 public:
     Particles particles;
 
+    /// @brief Set the particles to export to a file. This method is required before exporting.
+    /// @param particles 
     void setParticles(const Particles& particles);
+
+    /// @brief Export the particles to a file in the Prof format.
+    /// @param path path to the file to write
+    /// @param time current time in the simulation
     void toProf(const std::filesystem::path& path, const double& time);
+    /// @brief Export the particles to a file in the VTK zlib format.
+    /// @param path path to the file to write
+    /// @param time current time in the simulation
+    /// @param n0ForNumberDensity reference number density for the number density calculation
     void toVtu(const std::filesystem::path& path, const double& time, const double& n0ForNumberDensity = 1.0);
+    /// @brief Export the particles to a file in the CSV format.
+    /// @param path path to the file to write
+    /// @param time current time in the simulation
     void toCsv(const std::filesystem::path& path, const double& time);
 };
