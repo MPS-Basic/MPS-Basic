9d7625c4808e4ae90b19a81057c5b9f73cde0f8d
Kazuki Arai <zukain5@gmail.com>
2024-10-30 10:35:06 +0900

    Add scripts/safe_runner.sh

---
+++ scripts/safe_runner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Runner that seplates the result into a new folder with a timestamp
+
+# set variables
+projectName="dambreak"
+inputDir="input/${projectName}"
+settingFile="${inputDir}/settings.yml"
+
+timestamp=$(date +%Y%m%d%H%M%S)
+outputDir="result/${timestamp}"
+errorLogFile="${outputDir}/error.log"
+consoleLogFile="${outputDir}/console.log"
+
+# go to root directory so that we can activate the script from any directory
+cd $(dirname $0)/..
+
+# create output directory if not exist
+mkdir -p ${outputDir}
+
+# run simulation
+./build/mps --setting ${settingFile} --output ${outputDir} 2> ${errorLogFile} | tee ${consoleLogFile}
