537fd26dde86d889bf19719e9d584dc218fcea8a
tak948 <kushiya-takeshi393@g.ecc.u-tokyo.ac.jp>
2026-01-28 16:36:31 +0900

    fix vtu loader

--- src/particles_loader/vtu.cpp
+++ src/particles_loader/vtu.cpp
@@ -20,7 +20,20 @@ std::pair<double, Particles> Vtu::load(const fs::path& path, double defaultDensi
         std::exit(-1);
     }
 
+    // Check if the file contains "<AppendedData>" tag
     std::string line;
+    while (std::getline(ifs, line)) {
+        if (line.find("<AppendedData") != std::string::npos) {
+            cerr << "Error: Binary VTU files are not supported: " << fs::absolute(path) << endl;
+            std::exit(-1);
+        }
+    }
+
+    // Reset the file stream to the beginning for further processing
+    ifs.clear();
+    ifs.seekg(0, std::ios::beg);
+
+    // Load start
     double startTime = NAN;
     Particles particles;
 
@@ -32,58 +45,68 @@ std::pair<double, Particles> Vtu::load(const fs::path& path, double defaultDensi
 
     // Parse VTU file
     while (std::getline(ifs, line)) {
-        // Extract time from FieldData
-        if (line.find("<DataArray type='Float64' Name='Time'") != std::string::npos) {
+        // Extract start time
+        if (line.find("Time") != std::string::npos) {
             std::getline(ifs, line);
             startTime = std::stod(line);
         }
 
         // Extract particle positions
-        if (line.find("<DataArray NumberOfComponents='3' type='Float64' Name='position'") != std::string::npos) {
+        if (line.find("Position") != std::string::npos) {
             while (std::getline(ifs, line) && line.find("</DataArray>") == std::string::npos) {
                 std::istringstream iss(line);
                 double x, y, z;
-                iss >> x >> y >> z;
+                if (!(iss >> x >> y >> z)) {
+                    throw std::runtime_error("Invalid position data format");
+                }
                 positions.emplace_back(x, y, z);
             }
         }
 
         // Extract velocities
-        if (line.find("<DataArray NumberOfComponents='3' type='Float64' Name='Velocity'") != std::string::npos) {
+        if (line.find("Velocity") != std::string::npos) {
             while (std::getline(ifs, line) && line.find("</DataArray>") == std::string::npos) {
                 std::istringstream iss(line);
                 double vx, vy, vz;
-                iss >> vx >> vy >> vz;
+                if (!(iss >> vx >> vy >> vz)) {
+                    throw std::runtime_error("Invalid velocity data format");
+                }
                 velocities.emplace_back(vx, vy, vz);
             }
         }
 
         // Extract particle types
-        if (line.find("<DataArray NumberOfComponents='1' type='Int32' Name='Particle Type'") != std::string::npos) {
+        if (line.find("Particle Type") != std::string::npos) {
             while (std::getline(ifs, line) && line.find("</DataArray>") == std::string::npos) {
                 int type;
                 std::istringstream iss(line);
-                iss >> type;
+                if (!(iss >> type)) {
+                    throw std::runtime_error("Invalid particle type data format");
+                }
                 types.push_back(type);
             }
         }
 
         // Extract fluid types
-        if (line.find("<DataArray NumberOfComponents='1' type='Int32' Name='Fluid Type'") != std::string::npos) {
+        if (line.find("Fluid Type") != std::string::npos) {
             while (std::getline(ifs, line) && line.find("</DataArray>") == std::string::npos) {
                 int fluidType;
                 std::istringstream iss(line);
-                iss >> fluidType;
+                if (!(iss >> fluidType)) {
+                    throw std::runtime_error("Invalid fluid type data format");
+                }
                 fluidTypes.push_back(fluidType);
             }
         }
 
         // Extract densities
-        if (line.find("<DataArray NumberOfComponents='1' type='Float64' Name='Density'") != std::string::npos) {
+        if (line.find("Density") != std::string::npos) {
             while (std::getline(ifs, line) && line.find("</DataArray>") == std::string::npos) {
                 double density;
                 std::istringstream iss(line);
-                iss >> density;
+                if (!(iss >> density)) {
+                    throw std::runtime_error("Invalid density data format");
+                }
                 densities.push_back(density);
             }
         }
